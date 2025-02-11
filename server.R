3b37817ef4295b9b4332937e3d7618c116f470f1
Lubomír Štěpánek <lubomir.stepanek@gmail.com>
2025-02-11 02:14:10 +0100

    Tab with summary plots added, including their possible download
    I have added a new tab with summary plots, i.e., either histograms for numerical variables, or barplots for categorical varaibles. The plots can be downloaded by an end user.

--- server.R
+++ server.R
@@ -275,6 +275,173 @@ my_server <- function(
         
     })
     
+    output$plot_section_label <- renderUI({
+        
+        req(my_data())
+        h4("Variable distributions")
+        
+    })
+    
+    
+    # reactive function to get variable types ---------------------------------
+    
+    variable_types <- reactive({
+        
+        req(my_data())
+        data <- my_data()
+        data.frame(
+            Variable = colnames(data),
+            Data_Type = sapply(data, function(col) class(col)[1]),
+            stringsAsFactors = FALSE
+        )
+        
+    })
+    
+    
+    # Generate plots and download buttons for each variable dynamically -------
+    
+    output$variable_plots <- renderUI({
+        
+        req(my_data())
+        data <- my_data()
+        var_types <- variable_types()
+        
+        plot_list <- lapply(
+            seq_along(var_types$Variable),
+            function(i){
+                var_name <- var_types$Variable[i]
+                plot_output_id <- paste0("plot_", var_name)
+                download_button_id <- paste0("download_", var_name)
+                
+                tagList(
+                    div(
+                        style = "margin-top: 20px;", 
+                        h5(var_name),
+                        plotOutput(
+                            outputId = plot_output_id,
+                            height = "300px"
+                        ),
+                        downloadButton(
+                            download_button_id,
+                            "Download Plot",
+                            class = "btn btn-primary"
+                        )
+                    )
+                )
+            }
+        )
+        
+        do.call(tagList, plot_list)
+        
+    })
+    
+    # generate individual plots and download handlers -------------------------
+    
+    observe({
+        
+        req(my_data())
+        data <- my_data()
+        var_types <- variable_types()
+        
+        for(
+            i in seq_along(var_types$Variable)
+        ){
+            local({
+                var_name <- var_types$Variable[i]
+                var_type <- var_types$Data_Type[i]
+                plot_output_id <- paste0("plot_", var_name)
+                download_button_id <- paste0("download_", var_name)
+                
+                output[[plot_output_id]] <- renderPlot({
+                    
+                    req(data[[var_name]])
+                    
+                    if(
+                        var_type %in% c("integer", "numeric", "Date")
+                    ){
+                        
+                        # histogram for numeric and date variables
+                        hist(
+                            data[[var_name]], 
+                            col = "lightblue", 
+                            border = "black", 
+                            main = paste("histogram of", var_name), 
+                            xlab = var_name, 
+                            ylab = "frequency"
+                        )
+                        
+                    }else if(
+                        var_type %in% c("character", "logical", "factor")
+                    ){
+                        
+                        # barplot for categorical variables
+                        counts <- table(data[[var_name]])
+                        
+                        barplot(
+                            counts, 
+                            col = "orange", 
+                            border = "black", 
+                            main = paste("barplot of", var_name), 
+                            xlab = var_name, 
+                            ylab = "count",
+                            las = 2
+                        )
+                        
+                    }
+                    
+                })
+                
+                # download handler for each plot                
+                output[[download_button_id]] <- downloadHandler(
+                    
+                    filename = function(){
+                        paste("plot_", var_name, ".png", sep = "")
+                    },
+                    content = function(file){
+                        png(file, width = 800, height = 600)
+                        if(
+                            var_type %in% c("integer", "numeric", "Date")
+                        ){
+                            
+                            hist(
+                                data[[var_name]], 
+                                col = "lightblue", 
+                                border = "black", 
+                                main = paste("histogram of", var_name), 
+                                xlab = var_name, 
+                                ylab = "frequency"
+                            )
+                            
+                        }else if(
+                            var_type %in% c("character", "logical", "factor")
+                        ){
+                            
+                            counts <- table(data[[var_name]])
+                            
+                            barplot(
+                                counts, 
+                                col = "orange", 
+                                border = "black", 
+                                main = paste("barplot of", var_name), 
+                                xlab = var_name, 
+                                ylab = "count",
+                                las = 2
+                            )
+                            
+                        }
+                        
+                        dev.off()
+                        
+                    }
+                    
+                )
+                
+            })
+            
+        }
+        
+    })
+    
     
     ## ------------------------------------------------------------------------
     
