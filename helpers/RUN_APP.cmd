15da3afb2c7fa6a77c16d3732bba9178f1471bb1
Milan <cizek.milan@seznam.cz>
2025-04-02 22:32:54 +0200

    improved helper script for launching the application on Windows, added error handling

--- helpers/RUN_APP.cmd
+++ helpers/RUN_APP.cmd
@@ -1,11 +1,17 @@
 @echo off
+setlocal
 
-set R_PATH=
+set "R_PATH="
 
-for /f "delims=" %%i in ('dir "%ProgramFiles%\R" /ad /b') do set R_PATH=%ProgramFiles%\R\%%i
+for /f "delims=" %%i in ('dir "%ProgramFiles%\R" /ad /b /o-n') do (
+    if not defined R_PATH set "R_PATH=%ProgramFiles%\R\%%i"
+)
 
-echo %R_PATH%
+echo R is located at: "%R_PATH%"
 
-"%R_PATH%\bin\Rscript" -e "shiny::runApp('../', launch.browser = TRUE)"
+if not exist "%R_PATH%\bin\Rscript.exe" (
+    echo Error: Rscript.exe not found in "%R_PATH%\bin"
+    exit /b 1
+)
 
-pause
+"%R_PATH%\bin\Rscript.exe" -e "shiny::runApp('../', launch.browser = TRUE)"
