ac0fe718fe28a5832b4f3c1cea375017e0aec531
Milan <cizek.milan@seznam.cz>
2025-04-10 00:24:23 +0200

    added check for Rscript availability using 'command' in Linux script

--- helpers/RUN_APP.sh
+++ helpers/RUN_APP.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+if ! command -v Rscript &> /dev/null
+then
+    echo "Error: Rscript not found. Please install R."
+    exit 1
+fi
+
 Rscript -e "shiny::runApp('../', launch.browser = TRUE)"
