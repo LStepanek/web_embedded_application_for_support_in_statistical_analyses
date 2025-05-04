1b5ecfae5b3f6a98ec01610f6d304a60f8009d32
Milan <cizek.milan@seznam.cz>
2025-05-05 01:39:20 +0200

    Extract built-in dataset loading into a standalone function for better readability (refactoring)

--- functions.R
+++ functions.R
@@ -23,6 +23,21 @@ install_and_load_packages <- function(packages) {
 }
 
 
+# Get a named list of all supported data files from a directory
+get_builtin_named_datasets <- function(dir_path = SAMPLE_DATASETS_DIR, allowed_exts = ALLOWED_DATA_FILES) {
+  all_files <- list.files(dir_path, full.names = FALSE)
+
+  file_names <- all_files[sapply(all_files, function(f) {
+    any(sapply(allowed_exts, function(ext) grepl(paste0(ext, "$"), f, ignore.case = TRUE)))
+  })]
+
+  display_names <- sub(paste0("(", paste(allowed_exts, collapse = "|"), ")$"), "", file_names)
+  named_choices <- setNames(file_names, display_names)
+
+  return(named_choices)
+}
+
+
 ## ----------------------------------------------------------------------------
 
 ###############################################################################
