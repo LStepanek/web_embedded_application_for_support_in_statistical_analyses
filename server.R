d505ba61aed4b452d611799141469d038f0d8ebc
Milan <cizek.milan@seznam.cz>
2025-05-05 02:10:01 +0200

    Implemented an enhancement to the datatable with Variable data types; adding a selectbox to each row for users to change the data type for each column

--- server.R
+++ server.R
@@ -243,21 +243,35 @@ my_server <- function(
     # render upload summary table ---------------------------------------------
     
     output$upload_summary_table <- renderDT({
-        
         req(upload_summary())
+
+        select_options <- c("integer", "numeric", "logical", "string", "character", "date")
+
         datatable(
-            upload_summary(),
-            options = list(
-                pageLength = 10, 
-                autoWidth = TRUE, 
-                searchHighlight = TRUE, 
-                dom = 'Bfrtip'
-            ),
-            rownames = FALSE,
-            colnames = c("variable", "data type")
-        )
-        
+          upload_summary(),
+          escape = FALSE,  
+          options = list(
+            pageLength = 10,
+            autoWidth = TRUE,
+            dom = 'Bfrtip',
+            ordering = FALSE,
+            columnDefs = list(list(
+              targets = 1,
+              render = JS(sprintf(
+                "function(data, type, row, meta) {
+                   var options = ['%s'];
+                   return renderDataTypeSelector(data, type, row, options);
+                 }",
+                paste(select_options, collapse = "','")
+              ))
+            ))
+          ),
+          rownames = FALSE,
+          colnames = c("Variable", "Data type"),
+          selection = "none"
+        )        
     })
+
     
     # dynamically show "data preview" label only if a dataset is available ----
     
--- www/functions.js
+++ www/functions.js
@@ -14,7 +14,7 @@ function setNavbarStatisticMethodsState(enabled) {
 }
 
 
-// Function to highlight table cells that contain the value "N/A"
+// Function to highlight table cells that contain the value "N/A" (missings)
 function highlightMissingCells(row, data, index) {
   for (var i = 0; i < data.length; i++) {
     if (data[i] === "N/A") {
@@ -24,10 +24,57 @@ function highlightMissingCells(row, data, index) {
 }
 
 
+// Function to update the abbreviation string using the first character of each selected value
+function getSelectedDataTypes() {
+  var dataTypes = '';
+
+  // Iterate over all <select> elements within the DataTable
+  $('#upload_summary_table').find('select').each(function() {
+    var value = $(this).val().charAt(0).toUpperCase();
+
+    // Only include characters that match allowed data type initials
+    if (value && /^[INLSCD]$/.test(value)) {
+      dataTypes += value;
+    }
+  });
+
+  return dataTypes;
+}
+
+// Triggered when a select box value is changed
+function onDataTypeSelectChange(columnName, newType) {
+  var dataTypes = getSelectedDataTypes();
+
+  // Display a toast notification
+  //toastr.info('Data type for column "' + columnName + '" was changed to: ' + newType + '.');
+
+  // Update the visible input field so the user can see the change
+  document.getElementById("col_types").value = dataTypes;
+  // Notify Shiny about the new value
+  Shiny.setInputValue("col_types", dataTypes, {priority: "event"});
+}
+
+// Renders a <select> dropdown for choosing the data type of a table column.
+function renderDataTypeSelector(data, type, row, options) {
+  if (type === 'display') {
+    var select = '<select onchange="onDataTypeSelectChange(\'' + row[0] + '\', this.value)">';
+    for (var i = 0; i < options.length; i++) {
+      var value = options[i].trim();
+      var selected = value === String(data).trim() ? ' selected' : '';
+      select += '<option value="' + value + '"' + selected + '>' + value + '</option>';
+    }
+    select += '</select>';
+    return select;
+  }
+
+  return data;
+}
+
+
 $(document).ready(function() {
   toastr.options = {
       "closeButton": true,
-      "debug": false,
+      "debug": true,
       "newestOnTop": false,
       "progressBar": true,
       "preventDuplicates": true,
@@ -43,9 +90,11 @@ $(document).ready(function() {
       "hideMethod": "fadeOut"
   };
 
+
   // allow Shiny to call this function
   Shiny.addCustomMessageHandler("setNavbarStatisticMethodsState", function(enabled) {
     setNavbarStatisticMethodsState(enabled);
   });
+
 });
 
